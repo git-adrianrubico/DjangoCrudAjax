c443b0bd28eea249f6d5a00ce96c394e8afe0ed6
Adrian Rubico <adrianrubico@icloud.com>
2025-02-26 14:08:22 +0800

    Update requirements.txt & update DB Settings

--- Core/settings.py
+++ Core/settings.py
@@ -80,26 +80,26 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default" : {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME" : BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': '<your-database-name>',
-        'USER': '<your-database-user>',
-        'PASSWORD': '<your-database-password>',
-        'HOST': '<your-database-host>',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+    "default" : {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME" : BASE_DIR / "db.sqlite3",
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': '<your-database-name>',
+#         'USER': '<your-database-user>',
+#         'PASSWORD': '<your-database-password>',
+#         'HOST': '<your-database-host>',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#         },
+#     }
+# }
 
 
 # Password validation
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DjangoCrudAjax
+
+This is a example of Django CRUD with Ajax.
+
+Credit to the Owner: https://github.com/steve-njuguna-k/Django-Ajax-CRUD
